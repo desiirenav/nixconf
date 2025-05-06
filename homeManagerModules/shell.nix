0d224953ad6aef273576bcf703da1f2b6511fff3
desiirenav <desiirenav@hotmail.com>
2025-05-06 15:26:21 -0400

    starship update

--- homeManagerModules/shell.nix
+++ homeManagerModules/shell.nix
@@ -11,8 +11,37 @@
     '';
   };
 
-  programs.starship = lib.mkForce {
+  programs.starship = {
     enable = true;
     enableNushellIntegration = true;
+
+    settings = {
+      right_format = "$cmd_duration";
+
+      directory = {
+        format = "[ ](bold)[ $path ]($style)";
+        style = "bold";
+      };
+
+      character = {
+        success_symbol = "[ ](bold)[ ➜](bold)";
+        error_symbol = "[ ](bold)[ ➜](bold)";
+      };
+
+      cmd_duration = {
+        min_time = 10;
+        format = "[]($style)[[󰔚 ]()$duration]()[ ]($style)";
+        disabled = false;
+        style = "";
+      };
+
+      directory.substitutions = {
+        "~" = "󰋞";
+        "Documents" = " ";
+        "Downloads" = " ";
+        "Music" = " ";
+        "Pictures" = " ";
+      };
+    };
   };
 }
