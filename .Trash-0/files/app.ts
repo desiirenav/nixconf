53dd9c663809bcf9facd6e62d8af19a4e5546ce7
desiirenav <desiirenav@hotmail.com>
2025-05-21 04:52:32 -0400

    saving ricing progress

--- .Trash-0/files/app.ts
+++
@@ -1,8 +0,0 @@
-import { App } from "astal/gtk3";
-
-App.start({
-  main() {
-    // you will instantiate Widgets here
-    // and setup anything else if you need
-  },
-});
--- .Trash-0/files/ghostty.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  imports = [inputs.ghostty.homeModules.default];
-
-  programs.ghostty.enable = true;
-}
