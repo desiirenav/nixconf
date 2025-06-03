8f3d8e55c2c1aba2aceed1131955464a60880531
desiirenav <desiirenav@hotmail.com>
2025-06-03 00:00:13 -0400

    rose-pine-dawn in one line stylix

--- nixosModules/stylix.nix
+++ nixosModules/stylix.nix
@@ -18,25 +18,7 @@
       name = "BreezeX-RosePineDawn-Linux";
       size = 22;
     };
-
-    base16Scheme = {
-      base00 = "faf4ed";
-      base01 = "fffaf3";
-      base02 = "f2e9de";
-      base03 = "9893a5";
-      base04 = "797593";
-      base05 = "575279";
-      base06 = "575279";
-      base07 = "cecacd";
-      base08 = "b4637a";
-      base09 = "ea9d34";
-      base0A = "d7827e";
-      base0B = "286983";
-      base0C = "56949f";
-      base0D = "907aa9";
-      base0E = "ea9d34";
-      base0F = "cecacd";
-    };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-dawn.yaml";
     fonts = {
       serif = {
         package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
