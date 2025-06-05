f1dcac6d87be29aa1d721060a32165aad82a575b
desiirenav <desiirenav@hotmail.com>
2025-06-05 17:30:06 -0400

    deleting trash

--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -6,6 +6,12 @@
 }: {
   programs.nushell = {
     enable = true;
+    extraConfig = ''
+      $env.config.show_banner = false
+      if $nu.is-login and (tty | str trim) == "/dev/tty1" {
+         exec niri-session
+      }
+    '';
   };
   programs.starship = {
     enable = true;
